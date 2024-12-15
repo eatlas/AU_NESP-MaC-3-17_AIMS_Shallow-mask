ca2c5e2608cbc5f6fe8132f2d28c5df99feb0e22
Eric Lawrey <E.Lawrey@aims.gov.au>
2024-12-16 09:35:47 +1000

    Fix dependancies and PRJ WKT2 format changes

--- 02-download-input-data.py
+++ 02-download-input-data.py
@@ -1,4 +1,5 @@
 from data_downloader import DataDownloader
+from pyproj import CRS
 import os
 
 # Create an instance of the DataDownloader class
@@ -7,6 +8,17 @@ VERSION = '1-1'
 
 print("Downloading source data files. This will take a while ...")
 
+# --------------------------------------------------------
+# Function to replace the .prj file with WKT2 EPSG:4283
+def replace_prj_with_epsg_4283(shapefile_path):
+    prj_file = os.path.splitext(shapefile_path)[0] + ".prj"
+    print(f"Replacing .prj file at {prj_file} with EPSG:4283 WKT2...")
+    # Generate WKT2 string for EPSG:4283 using pyproj
+    wkt_content = CRS.from_epsg(4283).to_wkt("WKT2_2019")
+    with open(prj_file, "w") as f:
+        f.write(wkt_content)
+    print(".prj file successfully replaced!")
+    
 # --------------------------------------------------------
 # Australian Coastline 50K 2024 (NESP MaC 3.17, AIMS)
 # https://eatlas.org.au/geonetwork/srv/eng/catalog.search#/metadata/c5438e91-20bf-4253-a006-9e9600981c5f
@@ -21,6 +33,11 @@ downloader.download_and_unzip(direct_download_url, 'AU_AIMS_Coastline_50k_2024',
 #Lawrey, E. P., Stewart M. (2016) Complete Great Barrier Reef (GBR) Reef and Island Feature boundaries including Torres Strait (NESP TWQ 3.13, AIMS, TSRA, GBRMPA) [Dataset]. Australian Institute of Marine Science (AIMS), Torres Strait Regional Authority (TSRA), Great Barrier Reef Marine Park Authority [producer]. eAtlas Repository [distributor]. https://eatlas.org.au/data/uuid/d2396b2c-68d4-4f4b-aab0-52f7bc4a81f5
 direct_download_url = 'https://nextcloud.eatlas.org.au/s/xQ8neGxxCbgWGSd/download/TS_AIMS_NESP_Torres_Strait_Features_V1b_with_GBR_Features.zip'
 downloader.download_and_unzip(direct_download_url, 'GBR_AIMS_Complete-GBR-feat_V1b')
+
+# Replace the .prj file for the GBR shapefile because it is using an older WKT format
+# that fails to load correctly in stages 3.
+gbr_shapefile = os.path.join(downloader.download_path, 'GBR_AIMS_Complete-GBR-feat_V1b', 'TS_AIMS_NESP_Torres_Strait_Features_V1b_with_GBR_Features.shp')
+replace_prj_with_epsg_4283(gbr_shapefile)
 
 downloader.download_path = "in-data"
 # --------------------------------------------------------
--- 03-make-rough-mask-with-gbr.py
+++ 03-make-rough-mask-with-gbr.py
@@ -1,6 +1,7 @@
 import os
 import geopandas as gpd
 import pandas as pd
+from imageutil import TimePrint
 """
 This script combines two shapefiles to create a unified rough mask of shallow areas 
 and reefs for Northern Australia and the Great Barrier Reef (GBR). The process includes 
@@ -21,79 +22,100 @@ def main():
     input2 = 'in-data/AU_Rough-reef-shallow-mask/AU_AIMS_NESP-MaC-3-17_Rough-reef-shallow-mask_Base.shp'
     output_path = 'working-data/03-rough-reef-mask_poly/AU_Rough-reef-shallow-mask-with-GBR.shp'
     
+    t = TimePrint()
     # Create output directory if it doesn't exist
     output_dir = os.path.dirname(output_path)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-        print(f"Created output directory: {output_dir}")
+        t.print(f"Created output directory: {output_dir}")
     
-    print("Loading GBR features shapefile...")
+    t.print("Loading GBR features shapefile...")
     gbr = gpd.read_file(input1)
     
+    # The GBR shapefile has an older PRJ file that breaks the loading of the shapefile, 
+    # in newer versions of GeoPandas so in the 02-download script we replace it with a newer 
+    # WKT2 format prj file. 
+    # Even though it is updated read_file doesn't recognise the prj and returns
+    # with a CRS of None. So we manually set it here as well. This is apparently a problem
+    # with the interaction between fiona and GDAL. We need two hacks to get the file
+    # to load properly.    
+    gbr = gbr.set_crs("EPSG:4283")
+    
+    # Check and print the CRS
+    if gbr.crs is None:
+        t.print("CRS is not set. The shapefile may have failed to load the .prj file.")
+    else:
+        t.print(f"CRS detected: {gbr.crs}")
+    
     # Remove the main land feature from the mask
-    print("Filtering out mainland features...")
+    t.print("Filtering out mainland features...")
     gbr = gbr[gbr['FEAT_NAME'] != 'Mainland']
     
     # Buffer by a small amount to merge features close together
     # A 0.001 degree buffer corresponds to approximately 110 m
     # buffer.
-    print("Applying small positive buffer...")
+    t.print("Applying small positive buffer...")
     gbr['geometry'] = gbr['geometry'].buffer(0.001)
     
     # Dissolve to combine any overlapping features together
-    print("Dissolving shapes...")
+    t.print("Dissolving shapes...")
     gbr = gbr.dissolve()
     
     # Negative buffer to strink back to almost normal size.
     # Make it slightly less so there is a small buffer around each
     # feature to cover small errors in the masking. 
-    print("Applying negative buffer ...")
+    t.print("Applying negative buffer. This can take 15 min ...")
     gbr['geometry'] = gbr['geometry'].buffer(-0.0009)
     
     # Simplify with Douglas-Peucker 0.0002 degrees. This makes the 
     # reef boundaries a similar vertex density as the northern Australian
     # shallow reef mask. This simplification makes the dataset smaller
     # and allows the rendering of the mask faster.
-    print("Simplifying geometry...")
+    t.print("Simplifying geometry...")
     gbr['geometry'] = gbr['geometry'].simplify(0.0002)
     
     # By default the dissolve makes everything a single multi-part polygon
     # This makes performance poor as no polygons can be excluded by their
     # bounding box. Converting to single part splits the data back into
     # many separate polygons, improving performance.
-    print("Converting multi-part geometries to single-part...")
+    t.print("Converting multi-part geometries to single-part...")
     gbr = gbr.explode(index_parts=False)
     
     # Load the manual rough masking for Northern Australia
-    print("Loading Rough reef mask shapefile...")
+    t.print("Loading Rough reef mask shapefile...")
     rough_reef = gpd.read_file(input2)
+    # Check and print the CRS
+    if rough_reef.crs is None:
+        t.print("CRS is not set. The shapefile may have failed to load the .prj file.")
+    else:
+        t.print(f"CRS detected: {rough_reef.crs}")
     
     # Ensure both datasets are in the same CRS (EPSG:4326). GBR is GDA94.
-    print("Reprojecting datasets to EPSG:4326...")
+    t.print("Reprojecting datasets to EPSG:4326...")
     gbr = gbr.to_crs(epsg=4326)
     rough_reef = rough_reef.to_crs(epsg=4326)
     
     # Merge all features into one dataset. The rough masking will overlap
     # with parts of the GBR mapping in Torres Strait and GBR.
-    print("Merging datasets...")
+    t.print("Merging datasets...")
     combined = gpd.GeoDataFrame(pd.concat([rough_reef, gbr], ignore_index=True), crs=rough_reef.crs)
     
     # Get rid of overlaps
-    print("Dissolving all features to eliminate overlaps...")
+    t.print("Dissolving all features to eliminate overlaps...")
     combined = combined.dissolve()
     
     # Multi-part to single-part to improve performance
-    print("Converting multi-part geometries to single-part for improved performance...")
+    t.print("Converting multi-part geometries to single-part for improved performance...")
     combined = combined.explode(index_parts=False)
     
     # We are making a mask and so the attributes are meaningless and increase the file size.
-    print("Removing attributes, keeping only geometry...")
+    t.print("Removing attributes, keeping only geometry...")
     combined = combined[['geometry']]
     
     # Save as Rough-reef-mask-with-GBR
-    print(f"Saving output shapefile to {output_path}...")
+    t.print(f"Saving output shapefile to {output_path}...")
     combined.to_file(output_path)
-    print("Processing complete.")
+    t.print("Processing complete.")
 
 if __name__ == "__main__":
     main()
